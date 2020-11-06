af4fe5369b4abf5fdc4735b446ed7bdee33114eb
Karina Lazarina <karinyao27@gmail.com>
2020-11-06 15:23:06 +0200

    lesson1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="todo">
+    <p>TO DO:</p>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(task, index) in todoList" :key="index" v-on:click="checkItem()" >
+        {{task}}
+        <button v-on:click="removeItem(index)">Remove item</button>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <input type="text" v-model="newTask">
+    <button v-on:click="todoList.push(newTask)">Add task</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      todoList: ['do task1','do task2','do task3','do task4','do task5'],
+      newTask: ''
+    }
+  },
+  methods:{
+    removeItem(index){
+      this.todoList.splice(index,1)
+    },
+    checkItem(){
+      let list = document.querySelector('ul');
+      list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+          ev.target.classList.toggle('checked');
+        }
+      }, false);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+p{
+  font-family: "Arial Rounded MT Bold";
+  font-size: large;
+  color: cadetblue;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+li{
+  font-family: "Arial Rounded MT Bold";
+  font-size: medium;
+  color: cadetblue;
+  margin: 5px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+button{
+  background-color: cadetblue;
+  border: solid lightslategray 1px;
+  border-radius: 25px;
+  color: azure;
+  font-size: x-small;
 }
-a {
-  color: #42b983;
+input{
+  background-color: azure;
+  border: solid lightslategray 1px;
+  border-radius: 25px;
+  color : cadetblue;
+  margin: 5px;
+  padding: 5px;
 }
+ul li.checked {
+  text-decoration: line-through;
+}
+/*ul li.checked::before {*/
+/*  content: '';*/
+/*  position: absolute;*/
+/*  border-color: cadetblue;*/
+/*  border-style: solid;*/
+/*  border-width: 0 2px 2px 0;*/
+/*  top: 10px;*/
+/*  left: 16px;*/
+/*  transform: rotate(45deg);*/
+/*  height: 15px;*/
+/*  width: 7px;*/
+/*}*/
 </style>
